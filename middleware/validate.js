b5229cc683ddcddd0b22c0e472467becece24c1f
mkatopola <moffatkatopola@gmail.com>
2025-05-21 19:30:21 +0200

    validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -4,7 +4,6 @@ exports.validateBook = [
   body('title').notEmpty().withMessage('Title is required'),
   body('author').notEmpty().withMessage('Author is required'),
   body('ISBN').isISBN().withMessage('Invalid ISBN'),
-  // add more checks...
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
